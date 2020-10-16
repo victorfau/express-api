1a37e80e7cc4012f7522eea954480ce6c94913aa
vfa <victorrfau@gmail.com>
2020-10-16 15:14:40 +0200

    ajout security

--- router/users.js
+++ router/users.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', isDev, UserController.all)
 router.get('/:id', isDev, UserController.one)
 router.put('/:id', isAdmin, UserController.edit)
-router.post('/', isDev, UserController.create)
+router.post('/', UserController.create)
 router.delete('/:id', isAdmin, UserController.delete)
 
 router.post('/login', UserController.login);
